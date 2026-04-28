c703e07fe7e418b75b16687dab35bb7ff62f031d
coldspirit <coldspirit@gmail.com>
2026-04-28 01:31:46 +0100

    chore: cleanup unused repo clutter

--- _run_targeted_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH="$HOME/.local/bin:$PATH"
-cd /mnt/e/Dev/Filmu/FilmuCore
-uv run --extra dev pytest -q \
-  tests/test_show_completion.py \
-  tests/test_partial_show_requests.py \
-  tests/test_show_search_context.py \
-  tests/test_scrape_routes.py \
-  2>&1
